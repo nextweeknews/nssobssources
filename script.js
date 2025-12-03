bdbdff57564712c31f33f8b44e2acb5f1e042394
Aidan Kellaher <nextweekmedia@gmail.com>
2025-12-03 16:51:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const WORKER_URL = "https://your-worker-name.your-domain.workers.dev";
 
 const postConfig = {
-    client_id: "DISCORD|1069003073311211601"
+  "player_id": "aidankellaher",
+  "client_id": "DISCORD|1069003073311211601",
+  "leaderboard": "season_10",
+  "rating_type": "player_global_all"
 };
 
 async function fetchLeaderboard() {
