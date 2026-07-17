d2e7fe270bac3d1871ba661f1a29585670545f7f
Aidan Kellaher <nextweekmedia@gmail.com>
2026-07-17 10:10:10 -0500

    Update teamup-config.js

--- teamup-config.js
+++ teamup-config.js
@@ -1,6 +1,6 @@
 window.TEAMUP_CONFIG = window.TEAMUP_CONFIG || Object.freeze({
-  CURRENT_LEADERBOARD_KEY: "Season_12",
-  CURRENT_LEADERBOARD_LABEL: "Season 12",
+  CURRENT_LEADERBOARD_KEY: "Season_13",
+  CURRENT_LEADERBOARD_LABEL: "Season 13",
   DEFAULT_FORMAT: "",
   WORKER_BASE_URL: "https://rapid-haze-012c.nextweekmedia.workers.dev"
 });
